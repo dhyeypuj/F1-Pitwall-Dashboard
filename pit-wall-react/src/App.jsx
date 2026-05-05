96992a2aa44390f5ba1fd7300b6e29fc7020a71e
dhyeypuj <dhyey.pujara23@st.niituniversity.in>
2026-05-06 04:33:03 +0530

    feat: compose dashboard layout using React components
    - render all section components inside Layout.jsx
    - integrate Layout into App.jsx
    - establish base component hierarchy

--- pit-wall-react/src/App.jsx
+++ pit-wall-react/src/App.jsx
@@ -1,6 +1,9 @@
+import Layout from './layout/Layout'
+
 function App() {
   return (
     <div className="app">
+      <Layout />
     </div>
   )
 }
--- pit-wall-react/src/layout/Layout.jsx
+++ pit-wall-react/src/layout/Layout.jsx
@@ -1,7 +1,29 @@
+import Ticker from '../components/Ticker'
+import Hero from '../components/Hero'
+import RaceHero from '../components/RaceHero'
+import CalendarStrip from '../components/CalendarStrip'
+import DriversStandings from '../components/DriversStandings'
+import ConstructorsStandings from '../components/ConstructorsStandings'
+import Podium from '../components/Podium'
+import NewsFeed from '../components/NewsFeed'
+import StatsRibbon from '../components/StatsRibbon'
+import Footer from '../components/Footer'
+
 const Layout = () => {
   return (
     <div>
-      Layout Placeholder
+      <Ticker />
+      <Hero />
+      <RaceHero />
+      <CalendarStrip />
+      <div>
+        <DriversStandings />
+        <ConstructorsStandings />
+        <Podium />
+        <NewsFeed />
+      </div>
+      <StatsRibbon />
+      <Footer />
     </div>
   )
 }
